b1b68683ccd9f2b22d3c033ad4ec9d5545c83156
Ajaws2022 <112523117+Ajaws2022@users.noreply.github.com>
2022-10-09 14:05:25 -0700

    created a form that will display if the game is completed properly

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -7,7 +7,7 @@ var questions = [
     {
         question: 'What is 2 - 2 equal to?',
         choices: [0,1,3,4],
-        answer: 2
+        answer: 0
     },
     {
         question: 'What is 2 * 2 equal to?',
@@ -26,17 +26,27 @@ var quizEl = document.querySelector('#quiz-question');
 var choicesEl = document.querySelector('.quiz-choices');
 var confirmEl = document.querySelector('#confirm')
 var timerEl = document.querySelector('.timeBox')
+var initialForm = document.querySelector('#submitInitials')
+var secLeft = 20;
+
 // var  startingQuestionIndex = 0;
 
 var currentQuestionIndex = 0;
+// hides initial input form
+initialForm.style.display = "none";
 // populates a quiz question at the top of the page
 function generateQuestion(){
     quizEl.innerHTML = "";
     choicesEl.innerHTML = "";
     if(currentQuestionIndex >= questions.length){
         var h1Element = document.createElement('h1');
+        
     h1Element.textContent = "You've reached the end of the game!";
+    initialForm.style.display= "block";
+    confirmEl.textContent = '';
+    
     quizEl.append(h1Element);
+    
     return;
 
     }
@@ -56,14 +66,14 @@ function generateQuestion(){
    
 }
 
-var secLeft = 20;
+
 // This functions logic ensures that either the game is completed with the timer paused or the game is lost (ran out of time) and a 'game over' screen displayed.
 function setTimer(){
     var timerInterval = setInterval(function(){
         secLeft--;
         timerEl.textContent = secLeft;
     
-        if(secLeft <= 0 && currentQuestionIndex < questions.length) {
+        if(secLeft === 0 && currentQuestionIndex < questions.length) {
           // Stops execution of action at set interval
           clearInterval(timerInterval);
           var h1Element = document.createElement('h1');
@@ -75,7 +85,21 @@ function setTimer(){
             parElement.textContent = "You ran out of time.";
             quizEl.append(h1Element);
             choicesEl.append(parElement);
-        } if(secLeft > 0 && currentQuestionIndex === questions.length) {
+        } if(secLeft === 0 && currentQuestionIndex === questions.length) {
+            
+            clearInterval(timerInterval);
+            var h1Element = document.createElement('h1');
+            var parElement = document.createElement('p')
+  
+            quizEl.textContent = '';
+            choicesEl.textContent = '';
+              h1Element.textContent = "Game Over!";
+              parElement.textContent = "You ran out of time.";
+              quizEl.append(h1Element);
+              choicesEl.append(parElement);
+        }
+            
+        if(secLeft > 0 && currentQuestionIndex === questions.length) {
             clearInterval(timerInterval);
             
         }
@@ -99,7 +123,11 @@ function checkAnswer(event){
     generateQuestion();
 }
 
+
 buttonEl.addEventListener('click', setTimer);
 buttonEl.addEventListener('click', generateQuestion);
+buttonEl.addEventListener('click', () => {
+    buttonEl.style.display = 'none';
+});
 
 choicesEl.addEventListener('click', checkAnswer);
